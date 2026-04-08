97523a307115a5c84531710b9ff7dbe40dce9933
Abhishek Suryawanshi <abhisuryawanshi99@gmail.com>
2026-04-08 13:07:13 +0000

    get location added for webapp

--- main.py
+++ main.py
@@ -7,15 +7,24 @@ class Location(BaseModel):
     latitude: float
     longitude: float
 
+# store latest location
+latest_location = {"lat": 0, "lon": 0}
+
 @app.get("/")
 def home():
     return {"message": "API is running"}
 
 @app.post("/location")
 def receive_location(loc: Location):
-    print(f"Received: {loc.latitude}, {loc.longitude}")
-    return {
-        "status": "success",
+    global latest_location
+
+    latest_location = {
         "lat": loc.latitude,
         "lon": loc.longitude
     }
+
+    return {"status": "ok"}
+
+@app.get("/location")
+def get_location():
+    return latest_location
