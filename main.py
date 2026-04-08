a464d906d607bc8f3b86f3e9d6ecfb40c5058a0e
Abhishek Suryawanshi <abhisuryawanshi99@gmail.com>
2026-04-08 13:29:19 +0000

    cors added

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Location(BaseModel):
     latitude: float
     longitude: float
 
-# store latest location
 latest_location = {"lat": 0, "lon": 0}
 
 @app.get("/")
 def home():
     return {"message": "API is running"}
-
+    
 @app.post("/location")
 def receive_location(loc: Location):
     global latest_location
-
-    latest_location = {
-        "lat": loc.latitude,
-        "lon": loc.longitude
-    }
-
+    latest_location = {"lat": loc.latitude, "lon": loc.longitude}
     return {"status": "ok"}
 
 @app.get("/location")
